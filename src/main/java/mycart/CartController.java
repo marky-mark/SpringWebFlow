d21475f1ed0353c5598b62d1c3998db43d9c6cf5
mkelly <mkelly28@tcd.ie>
2012-07-19 14:18:25 +0100

    added some more flows

--- src/main/java/mycart/CartController.java
+++ src/main/java/mycart/CartController.java
@@ -1,12 +1,24 @@
 package mycart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CartController {
 
-    @RequestMapping("/home.do")
-    public void doHome() {
-    }
+	private CartService cartService;
+	
+	public void setCartService(CartService cartService) {
+		this.cartService = cartService;
+	}
+	
+	@RequestMapping("/home.do")
+	public ModelMap doHome() {
+		ModelMap model = new ModelMap();
+		model.addAttribute("shoppingCart", cartService.getShoppingCart());
+		model.addAttribute("products", cartService.getProducts());
+		return model;
+	}
 }
---
+++ src/main/java/mycart/CartService.java
@@ -0,0 +1,18 @@
+package mycart;
+
+import java.util.List;
+
+public interface CartService {
+	
+	ShoppingCart getShoppingCart();
+	
+	List<Product> getProducts();
+	
+	List<Product> getRecommendations();
+	
+	Product getProduct(long productId);
+	
+	List<String> getShippingOptions();
+	
+	void submitOrderForPayment();
+}
