f144eb13535475c4cf888aabd060f20a7d6c292e
Mark Kelly <mark@mkelly>
2012-07-19 12:40:11 +0100

    first commit

---
+++ src/main/java/mycart/CartController.java
@@ -0,0 +1,12 @@
+package mycart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CartController {
+
+    @RequestMapping("/home.do")
+    public void doHome() {
+    }
+}
